e76415c0171dac495ad9587ba6e07283c4b7d08f
Flavio Calado (Anima) <flavio.calado@animaeducacao.com.br>
2023-04-04 21:10:33 -0300

    Foi criado o arquivo da atividade 1 da prática 2

---
+++ Pratica2Atividade1.java
@@ -0,0 +1,28 @@
+/* Nome do Aluno:  Flavio Calado
+ * RA: 300009030
+ * Nome do Programa: Pratica2Atividade1 
+ * Descrição: Faça um programa que leia dois números A e B e imprima o maior deles.
+ * Data: 04/04/2023
+ */
+import java.util.Scanner;
+
+public class Pratica2Atividade1 {
+
+    public static void main(String[] args) {
+        int a, b;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero:");
+        a = teclado.nextInt();
+        System.out.println("Digite o segundo numero:");
+        b = teclado.nextInt();
+        teclado.close();
+
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+
+}
